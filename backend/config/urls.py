d16c7691371966e04de45a57e497beb748d1671e
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-24 16:59:39 +0600

    Include urls from api version app

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.urls')),
+    path('api/', include('apps.v1api.urls')),
 ]
