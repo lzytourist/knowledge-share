2f8da2e62aa329d9ca7d34a6d930fc56354d117e
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-24 19:42:10 +0600

    Rest framework

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework_simplejwt',
+
     'apps.users.apps.UsersConfig',
     'apps.workspaces.apps.WorkspacesConfig',
 ]
