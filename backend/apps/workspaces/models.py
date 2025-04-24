5273f23ecd612f36f547ac813fe05cd15c2ddf6e
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-24 19:31:53 +0600

    Workspace model

--- backend/apps/workspaces/models.py
+++ backend/apps/workspaces/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Workspace(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'workspaces'
+        indexes = [
+            models.Index(fields=['name']),
+        ]
