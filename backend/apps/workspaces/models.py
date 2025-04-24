087d2dbf56454c6a47b1cc6e14ecd27cc6dbeb2d
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-24 19:37:23 +0600

    Workspace role

--- backend/apps/workspaces/models.py
+++ backend/apps/workspaces/models.py
@@ -26,3 +26,14 @@ class Permission(models.Model):
 
     class Meta:
         db_table = 'permissions'
+
+
+class Role(models.Model):
+    label = models.CharField(max_length=100)
+    permissions = models.ManyToManyField(Permission)
+
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        db_table = 'workspace_roles'
