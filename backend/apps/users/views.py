67b2f33588eb007a5ae9d7a2a9828df1de956269
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-27 22:18:28 +0600

    User view set for admin and user profile management

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from apps.users.models import User
+from apps.users.serializers import UserSerializer, PasswordChangeSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+
+class UpdateProfileAPIView(APIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, instance=request.user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            data={'message': 'Profile updated successfully'},
+        )
+
+
+class PasswordChangeAPIView(APIView):
+    serializer_class = PasswordChangeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            data={'message': 'Password changed successfully'},
+        )
