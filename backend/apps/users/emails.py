dde180200292c956d8bc53f53fc3081202a1021e
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-28 19:29:23 +0600

    account created email with password change link

--- backend/apps/users/emails.py
+++ backend/apps/users/emails.py
@@ -5,12 +5,12 @@ from django.template.loader import render_to_string
 from apps.users.models import User
 
 
-def send_account_created_mail_with_reset_password_link(user: User, activation_token: str):
+def send_account_created_mail_with_reset_password_link(user_email: str, name: str, activation_token: str):
     email = EmailMessage(
-        to=[user.email],
+        to=[user_email],
         subject='Account Created',
         body=render_to_string('emails/users/account_created.html', {
-            'user': user,
+            'name': name,
             'activation_link': f'{settings.FRONTEND_URL}/account/activate/{activation_token}',
         })
     )
