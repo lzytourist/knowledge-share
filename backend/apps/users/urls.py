79274d610e2968a876956e0989521c72a36c1385
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-28 14:47:37 +0600

    User API routes and profile and password change route

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from apps.users.views import UserViewSet, UpdateProfileAPIView, PasswordChangeAPIView
+
+router = DefaultRouter()
+router.register('', UserViewSet, basename='user')
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+    path('profile/', UpdateProfileAPIView.as_view(), name='me'),
+    path('profile/password/', PasswordChangeAPIView.as_view(), name='password-change'),
+] + router.urls
