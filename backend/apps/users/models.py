fd8eeb9a2caa23d2da62eedc140fb751110f1e8e
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-28 14:52:46 +0600

    User is active field

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -44,6 +44,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
