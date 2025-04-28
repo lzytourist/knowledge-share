1d4216edd5a6c81109cce021c09ab183bec4d0b7
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-28 12:17:04 +0600

    Check circular dependency in serializer

--- backend/apps/workspaces/serializers.py
+++ backend/apps/workspaces/serializers.py
@@ -10,6 +10,14 @@ class WorkspaceSerializer(serializers.ModelSerializer):
 
 
 class PermissionSerializer(serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        depends_on_id = validated_data.get('depends_on')
+        if depends_on_id:
+            depends_on = Permission.objects.get_dependency_list(depends_on_id.pk if isinstance(depends_on_id, Permission) else depends_on_id)
+            if instance.id in depends_on.values_list('id', flat=True):
+                raise serializers.ValidationError({'depends_on': ['Circular dependency detected']})
+        return super().update(instance, validated_data)
+
     class Meta:
         model = Permission
         fields = '__all__'
