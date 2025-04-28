bac8ce4342759d4a6fe2de2451fa5c3417162bce
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-28 19:35:41 +0600

    remove unwanted import

--- backend/apps/users/emails.py
+++ backend/apps/users/emails.py
@@ -2,8 +2,6 @@ from django.conf import settings
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
-from apps.users.models import User
-
 
 def send_account_created_mail_with_reset_password_link(user_email: str, name: str, activation_token: str):
     email = EmailMessage(
