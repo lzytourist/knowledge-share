620c380ca522ee098fb59195f167cdbba495ee1e
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-28 19:29:43 +0600

    email sending via background task

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,15 +1,23 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.users.models import User
 from apps.users.serializers import UserSerializer, PasswordChangeSerializer
+from apps.users.tasks import send_activation_and_password_reset
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        if response.status_code == status.HTTP_201_CREATED:
+            send_activation_and_password_reset.delay(response.data['email'], response.data['name'])
+
+        return response
 
 
 class UpdateProfileAPIView(APIView):
