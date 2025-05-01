5ab5ad90319598eb0bbfa68007dc624b8cf3b1e8
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-05-01 15:11:31 +0600

    Activate account url

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from apps.users.views import UserViewSet, UpdateProfileAPIView, PasswordChangeAPIView
+from apps.users.views import UserViewSet, UpdateProfileAPIView, PasswordChangeAPIView, AccountActivationAPIView
 
 router = DefaultRouter()
 router.register('', UserViewSet, basename='user')
@@ -12,4 +12,5 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', UpdateProfileAPIView.as_view(), name='me'),
     path('profile/password/', PasswordChangeAPIView.as_view(), name='password-change'),
+    path('activate/', AccountActivationAPIView.as_view(), name='activate-account'),
 ] + router.urls
