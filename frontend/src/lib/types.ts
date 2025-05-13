34205152e3caa83ceec3c54aa61cf41155528932
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-05-13 15:02:13 +0600

    Login token type

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -2,3 +2,8 @@ import {z} from "zod";
 import {LoginSchema} from "@/lib/schemas";
 
 export type LoginSchemaType = z.infer<typeof LoginSchema>;
+
+export interface LoginTokenType {
+    access: string;
+    refresh: string;
+}
