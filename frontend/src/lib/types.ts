3fa43d26a777dbba63810c3baf66304bf5250e6a
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-05-13 21:45:09 +0600

    Error types

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -7,3 +7,17 @@ export interface LoginTokenType {
     access: string;
     refresh: string;
 }
+
+export interface FieldError {
+    [key: string]: string[]
+}
+
+export interface UnauthorizedError {
+    detail: string;
+    code: 'access' | 'refresh'
+}
+
+export interface ApiError {
+    status?: number;
+    data?: UnauthorizedError | FieldError;
+}
