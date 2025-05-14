35d8a0dd8c8fc842ac39c14cd238b0827e6ba049
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-05-14 13:08:38 +0600

    PROD-001: Password reset email

--- backend/apps/users/emails.py
+++ backend/apps/users/emails.py
@@ -14,3 +14,16 @@ def send_account_created_mail_with_reset_password_link(user_email: str, name: st
     )
     email.content_subtype = 'html'
     email.send(fail_silently=False)
+
+
+def send_password_reset_mail(user_email: str, name: str, token: str):
+    email = EmailMessage(
+        to=[user_email],
+        subject='Password Reset Requested',
+        body=render_to_string('emails/users/reset_password.html', {
+            'name': name,
+            'link': f'{settings.FRONTEND_URL}/password-reset/{token}',
+        })
+    )
+    email.content_subtype = 'html'
+    email.send(fail_silently=False)
